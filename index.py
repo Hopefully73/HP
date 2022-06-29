5209b38c5f8f06623057e13873b2b810b5918da6
unknown <gwyn.magoncia73@gmail.com>
2022-06-29 14:57:29 +0800

    Added google analytics code

--- index.py
+++ index.py
@@ -6,3 +6,31 @@ app.title = "Hero Park"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-999999-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-10SPPKS748');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
+
